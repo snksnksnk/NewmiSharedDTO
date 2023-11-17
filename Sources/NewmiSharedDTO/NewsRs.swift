4f263654dd03fec2f6efabb4caa543e10f6fa463
Demetris <snakeye-cy@hotmail.com>
2023-11-17 09:58:10 +0200

    Inital

---
+++ Sources/NewmiSharedDTO/NewsRs.swift
@@ -0,0 +1,30 @@
+//
+//  File.swift
+//  
+//
+//  Created by Demetris Georgiou on 17/11/2023.
+//
+
+import Foundation
+
+public struct NewsRs:Codable{
+    public var title:String?
+    public var date:String?
+    public var url:String?
+    public var source:String?
+    public var newsType:String?
+    public var categories:String?
+    public var imageUrl:String?
+    public var videoUrl:String?
+    
+    public init(title: String? = nil, date: String? = nil, url: String? = nil, source: String? = nil, newsType: String? = nil, categories: String? = nil, imageUrl: String? = nil, videoUrl: String? = nil) {
+        self.title = title
+        self.date = date
+        self.url = url
+        self.source = source
+        self.newsType = newsType
+        self.categories = categories
+        self.imageUrl = imageUrl
+        self.videoUrl = videoUrl
+    }
+}
