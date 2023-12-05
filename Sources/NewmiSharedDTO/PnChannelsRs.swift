38db6b2fd7e45b4f173b1c3b1116836333bff209
Demetris <snakeye-cy@hotmail.com>
2023-12-05 18:00:09 +0200

    added pushNotificationChannels

---
+++ Sources/NewmiSharedDTO/PnChannelsRs.swift
@@ -0,0 +1,34 @@
+//
+//  File.swift
+//  
+//
+//  Created by Demetris Georgiou on 05/12/2023.
+//
+
+import Foundation
+
+public struct PnChannelsRs:Codable{
+    public var channels:[PnChannel]?
+    
+    public init(channels: [PnChannel]? = nil) {
+        self.channels = channels
+    }
+}
+
+public struct PnChannel:Codable,Hashable{
+    public static func == (lhs: PnChannel, rhs: PnChannel) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+    public var id:UUID?
+    public var name:String?
+    public var channelCode:String?
+    public var isSelected:Bool?
+    
+    public init(id: UUID? = nil, name: String? = nil, channelCode:String? = nil, isSelected:Bool? = nil) {
+        self.id = id
+        self.name = name
+        self.channelCode = channelCode
+        self.isSelected = isSelected
+    }
+}
