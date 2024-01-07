57ef64aaec57e7d46b6fe6f521d1ad09a1b9125c
Demetris <snakeye-cy@hotmail.com>
2024-01-07 13:33:36 +0200

    changed NewsRq

--- Sources/NewmiSharedDTO/NewsRq.swift
+++ Sources/NewmiSharedDTO/NewsRq.swift
@@ -10,9 +10,13 @@ import Foundation
 public struct NewsRq:Codable{
     public var sources:[String]?
     public var categories:[String]?
+    public var newsType:String?
+    public var page:Int?
     
-    public init(sources: [String]? = nil, categories:[String]? = nil) {
+    public init(sources: [String]? = nil, categories:[String]? = nil, newsType:String? = nil, page:Int? = nil) {
         self.sources = sources
         self.categories = categories
+        self.newsType = newsType
+        self.page = page
     }
 }
