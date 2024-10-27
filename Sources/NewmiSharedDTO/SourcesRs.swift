5cae0d168dea59762c8402910fef971294a527ed
Demetris <snakeye-cy@hotmail.com>
2024-10-27 21:37:02 +0200

    Added last update and status to Sources

--- Sources/NewmiSharedDTO/SourcesRs.swift
+++ Sources/NewmiSharedDTO/SourcesRs.swift
@@ -15,6 +15,11 @@ public struct SourcesRs:Codable{
     }
 }
 
+public enum SourceStatus:String,Codable, Sendable{
+    case online = "ONLINE"
+    case offline = "OFFLINE"
+}
+
 public struct Source:Codable,Hashable, Sendable{
     public static func == (lhs: Source, rhs: Source) -> Bool {
         lhs.id == rhs.id
@@ -26,13 +31,17 @@ public struct Source:Codable,Hashable, Sendable{
     public var countryCode:String?
     public var sourceCode:String?
     public var isSelected:Bool?
+    public var lastUpdate:Date?
+    public var status:SourceStatus?
     
-    public init(id: UUID? = nil, name: String? = nil, language: String? = nil, countryCode: String? = nil, sourceCode:String? = nil, isSelected:Bool? = nil) {
+    public init(id: UUID? = nil, name: String? = nil, language: String? = nil, countryCode: String? = nil, sourceCode:String? = nil, isSelected:Bool? = nil, lastUpdate: Date? = nil, status: SourceStatus? = nil) {
         self.id = id
         self.name = name
         self.language = language
         self.countryCode = countryCode
         self.sourceCode = sourceCode
         self.isSelected = isSelected
+        self.lastUpdate = lastUpdate
+        self.status = status
     }
 }
