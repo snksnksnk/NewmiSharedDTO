342fd6771d8f1d5dfe12d2d09a73e81c535ed7f2
Demetris <snakeye-cy@hotmail.com>
2024-11-17 12:43:56 +0200

    Added Pharmacies

---
+++ Sources/NewmiSharedDTO/PharmaciesRs.swift
@@ -0,0 +1,46 @@
+//
+//  PharmaciesRs.swift
+//  NewmiSharedDTO
+//
+//  Created by Demetris Georgiou on 17/11/2024.
+//
+
+import Foundation
+
+public struct PharmaciesRs: Codable, Sendable{
+    public var pharmacies:[Pharmacy]?
+    
+    public init(pharmacies: [Pharmacy]? = nil) {
+        self.pharmacies = pharmacies
+    }
+}
+
+public struct Pharmacy: Codable, Hashable, Sendable{
+    public static func == (lhs: Pharmacy, rhs: Pharmacy) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+    public var id: UUID?
+    public var category: String?
+    public var name: String?
+    public var address: String?
+    public var phoneOffice: String?
+    public var phoneHome: String?
+    public var email: String?
+    public var address2: String?
+    public var postCode: String?
+    public var city: String?
+    
+    public init(id: UUID? = nil, category: String? = nil, name: String? = nil, address: String? = nil, phoneOffice: String? = nil, phoneHome: String? = nil, email: String? = nil, address2: String? = nil, postCode: String? = nil, city: String? = nil) {
+        self.id = id
+        self.category = category
+        self.name = name
+        self.address = address
+        self.phoneOffice = phoneOffice
+        self.phoneHome = phoneHome
+        self.email = email
+        self.address2 = address2
+        self.postCode = postCode
+        self.city = city
+    }
+}
