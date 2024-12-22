cef08bf910d4987b38b88d8157a86c6420a98cc1
Demetris <snakeye-cy@hotmail.com>
2024-12-22 14:26:46 +0200

    reverted previous commit changes

--- Sources/NewmiSharedDTO/NewsRq.swift
+++ Sources/NewmiSharedDTO/NewsRq.swift
@@ -12,13 +12,11 @@ public struct NewsRq: Codable, Sendable{
     public var categories: [String]?
     public var newsType: String?
     public var page: Int?
-    public var category: String?
     
-    public init(sources: [String]? = nil, categories: [String]? = nil, newsType: String? = nil, page: Int? = nil, category: String? = nil) {
+    public init(sources: [String]? = nil, categories: [String]? = nil, newsType: String? = nil, page: Int? = nil) {
         self.sources = sources
         self.categories = categories
         self.newsType = newsType
         self.page = page
-        self.category = category
     }
 }
