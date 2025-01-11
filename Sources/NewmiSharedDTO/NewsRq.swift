b7b68c57c5a0ab645aabc154b16d57d88a48c1ae
Demetris <snakeye-cy@hotmail.com>
2025-01-11 08:46:18 +0200

    Added Country code in NewsRq

--- Sources/NewmiSharedDTO/NewsRq.swift
+++ Sources/NewmiSharedDTO/NewsRq.swift
@@ -10,12 +10,14 @@ import Foundation
 public struct NewsRq: Codable, Sendable{
     public var sources: [String]?
     public var categories: [String]?
+    public var countryCode: String?
     public var newsType: String?
     public var page: Int?
     
-    public init(sources: [String]? = nil, categories: [String]? = nil, newsType: String? = nil, page: Int? = nil) {
+    public init(sources: [String]? = nil, categories: [String]? = nil, countryCode: String? = nil, newsType: String? = nil, page: Int? = nil) {
         self.sources = sources
         self.categories = categories
+        self.countryCode = countryCode
         self.newsType = newsType
         self.page = page
     }
