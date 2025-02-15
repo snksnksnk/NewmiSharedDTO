24d17f5ffc5a34162de419fb863b3cdb1110c481
Demetris <snakeye-cy@hotmail.com>
2025-02-15 09:46:54 +0200

    Added timeline in PnChannelRegister

--- Sources/NewmiSharedDTO/PnChannelsRegisterRq.swift
+++ Sources/NewmiSharedDTO/PnChannelsRegisterRq.swift
@@ -14,11 +14,13 @@ public struct PnChannelsRegisterRq: Codable, Sendable{
     public var pnToken: String?
     public var channels: [String]?
     public var deviceOS: String?
+    public var timeLine: [Int]?
     
-    public init(deviceIdentif: UUID? = nil, pnToken: String? = nil, channels: [String]? = nil, deviceOS: String? = nil) {
+    public init(deviceIdentif: UUID? = nil, pnToken: String? = nil, channels: [String]? = nil, deviceOS: String? = nil, timeLine: [Int]? = nil) {
         self.deviceIdentif = deviceIdentif
         self.pnToken = pnToken
         self.channels = channels
         self.deviceOS = deviceOS
+        self.timeLine = timeLine
     }
 }
