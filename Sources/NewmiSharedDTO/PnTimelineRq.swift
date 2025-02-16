0dc2473cdc34ff710890b00b326cfffaeca80da6
Demetris <snakeye-cy@hotmail.com>
2025-02-16 14:04:36 +0200

    Added DeviceId in PnTimelineRq

--- Sources/NewmiSharedDTO/PnTimelineRq.swift
+++ Sources/NewmiSharedDTO/PnTimelineRq.swift
@@ -9,10 +9,12 @@ import Foundation
 
 public struct PnTimelineRq: Codable, Sendable{
     
+    public var deviceId: UUID?
     public var timeLine: [CGFloat]?
     public var status: String?
     
-    public init(timeLine: [CGFloat]? = nil, status: String? = nil) {
+    public init(deviceId: UUID? = nil, timeLine: [CGFloat]? = nil, status: String? = nil) {
+        self.deviceId = deviceId
         self.timeLine = timeLine
         self.status = status
     }
